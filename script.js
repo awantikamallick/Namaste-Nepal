2f707a315d9e26fa689f5aaf80409f7348587480
Awantika Mallick <84182317+awantikamallick@users.noreply.github.com>
2022-08-23 22:22:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showP(image) { 
-    document.getElementById('sp').style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(' + image +') ';
-};
-function changeT(text){
-    document.getElementById('name').innerHTML = text
-}
